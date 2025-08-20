d58b3ac6bfd92b7cef835163ef49239d49d76b9a
yatihe01 <yatihe769@gmail.com>
2025-08-20 16:42:21 +0800

    Level 0.Rename, Greet, Exit

---
+++ src/main/java/ChatHYT.java
@@ -0,0 +1,7 @@
+public class ChatHYT {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm ChatHYT");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
