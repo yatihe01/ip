effa18a549031ce506ba75b4db38ad5fefb4f396
yatihe01 <yatihe769@gmail.com>
2025-08-20 16:53:22 +0800

    Level 1: Echo

--- src/main/java/ChatHYT.java
+++ src/main/java/ChatHYT.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
 public class ChatHYT {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Hello! I'm ChatHYT");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+        while(true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
