fffa28408e8d3d4f794f04f592c0ce8e3932aa6c
yatihe01 <yatihe769@gmail.com>
2025-08-20 17:35:45 +0800

    Level 2: Add, List

--- src/main/java/ChatHYT.java
+++ src/main/java/ChatHYT.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 public class ChatHYT {
     static Scanner scanner = new Scanner(System.in);
+    static String[] tasks = new String[100];
+    static int taskCount = 0;
     public static void main(String[] args) {
         System.out.println("Hello! I'm ChatHYT");
         System.out.println("What can I do for you?");
@@ -14,6 +16,25 @@ public class ChatHYT {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            }
+
+            if (input.equalsIgnoreCase("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+                continue;
+            }
+
+            if (taskCount < 100) {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + input);
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("Enough! Please do current tasks first!");
             }
 
             System.out.println("____________________________________________________________");
