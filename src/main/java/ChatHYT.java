a686ad68c8fc01c16fe44a5ddc75a288004a3a46
yatihe01 <yatihe769@gmail.com>
2025-08-22 13:12:06 +0800

    Level 5: Handle Errors

--- src/main/java/ChatHYT.java
+++ src/main/java/ChatHYT.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
+import exception.EmptyDescriptionException;
+import exception.InvalidCommandException;
+
 public class ChatHYT {
     static Scanner scanner = new Scanner(System.in);
     static Task[] tasks = new Task[100];
     static int taskCount = 0;
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm ChatHYT");
         System.out.println("What can I do for you?");
@@ -29,68 +33,101 @@ public class ChatHYT {
             }
 
             if (input.startsWith("mark ")) {
-                int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                if (index >=0 && index < taskCount) {
-                    tasks[index].markAsDone();
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("  " + tasks[index]);
-                    System.out.println("____________________________________________________________");
+                try {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (index >= 0 && index < taskCount) {
+                        tasks[index].markAsDone();
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("  " + tasks[index]);
+                        System.out.println("____________________________________________________________");
+                    } else {
+                        throw new InvalidCommandException("Invalid task number.");
+                    }
+                } catch (InvalidCommandException e) {
+                    System.out.println("OOPS!!! " + e.getMessage());
                 }
                 continue;
             }
 
             if (input.startsWith("unmark ")) {
-                int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                if (index >= 0 && index < taskCount) {
-                    tasks[index].unmarkAsDone();
+                try {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (index >= 0 && index < taskCount) {
+                        tasks[index].unmarkAsDone();
+                        System.out.println("____________________________________________________________");
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println("  " + tasks[index]);
+                        System.out.println("____________________________________________________________");
+                    } else {
+                        throw new InvalidCommandException("Invalid task number.");
+                    }
+                } catch (InvalidCommandException e) {
+                    System.out.println("OOPS!!! " + e.getMessage());
+                }
+                continue;
+            }
+
+            try {
+                if (taskCount >= 100) {
+                    System.out.println("Enough! Please do current tasks first!");
+                    continue;
+                }
+                if (input.startsWith("todo")) {
+                    String description = input.substring(4);
+                    if (description.isEmpty()) {
+                        throw new EmptyDescriptionException("Haha! You don't want to do anything right?");
+                    }
+                    tasks[taskCount] = new Todo(description);
+                    taskCount++;
                     System.out.println("____________________________________________________________");
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println("  " + tasks[index]);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + tasks[taskCount - 1]);
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+
+                } else if (input.startsWith("deadline")) {
+                    String input1 = input.substring(8);
+                    if (!input1.contains("/by")) {
+                        throw new InvalidCommandException("Deadline must have an end.");
+                    }
+                    String description = input1.split(" /by")[0];
+                    String by = input1.split(" /by")[1];
+                    tasks[taskCount] = new Deadline(description, by);
+                    taskCount++;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + tasks[taskCount - 1]);
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
+
+                } else if (input.startsWith("event")) {
+                    String input2 = input.substring(5);
+                    if (!input2.contains("from")||!input2.contains("to")) {
+                        throw new InvalidCommandException("Event must have a period.");
+                    }
+                    String description = input2.split(" /from")[0];
+                    String from_1 = input2.split("/from ")[1];
+                    String from = from_1.split(" /to")[0];
+                    String to = input2.split("/to ")[1];
+                    tasks[taskCount] = new Event(description, from, to);
+                    taskCount++;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + tasks[taskCount - 1]);
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
                     System.out.println("____________________________________________________________");
                 }
-            }
+                else {
+                    throw new InvalidCommandException("What?");
+                }
+            } catch (EmptyDescriptionException | InvalidCommandException e) {
+                System.out.println("____________________________________________________________");
+                System.out.println(e.getMessage());
+                System.out.println("____________________________________________________________");
 
-            if (taskCount < 100 && input.startsWith("todo ")) {
-                String description = input.substring(4);
-                tasks[taskCount] = new Todo(description);
-                taskCount++;
-                System.out.println("____________________________________________________________");
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + tasks[taskCount - 1]);
-                System.out.println("Now you have " + taskCount + " tasks in the list.");
-                System.out.println("____________________________________________________________");
-            }
-
-            if (taskCount < 100 && input.startsWith("deadline ")) {
-                String input1 = input.substring(8);
-                String description = input1.split(" /by")[0];
-                String by = input1.split(" /by")[1];
-                tasks[taskCount] = new Deadline(description, by);
-                taskCount++;
-                System.out.println("____________________________________________________________");
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + tasks[taskCount - 1]);
-                System.out.println("Now you have " + taskCount + " tasks in the list.");
-                System.out.println("____________________________________________________________");
-            }
-
-            if (taskCount < 100 && input.startsWith("event ")) {
-                String input2 = input.substring(5);
-                String description = input2.split(" /from")[0];
-                String from_1 = input2.split("/from ")[1];
-                String from = from_1.split(" /to")[0];
-                String to = input2.split("/to ")[1];
-                tasks[taskCount] = new Event(description, from, to);
-                taskCount++;
-                System.out.println("____________________________________________________________");
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + tasks[taskCount - 1]);
-                System.out.println("Now you have " + taskCount + " tasks in the list.");
-                System.out.println("____________________________________________________________");
             }
         }
-
         scanner.close();
     }
 }
