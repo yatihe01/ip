6ccca9efafb3af748e3d36a594fe40fe227476a6
yatihe01 <yatihe769@gmail.com>
2025-09-05 14:46:09 +0800

    Level 10: GUI

--- src/main/java/duke/ChatHYT.java
+++ src/main/java/duke/ChatHYT.java
@@ -2,138 +2,47 @@ package duke;
 
 import java.util.Scanner;
 import java.util.ArrayList;
+
+import duke.command.Command;
 import duke.exception.EmptyDescriptionException;
 import duke.exception.InvalidCommandException;
 
 public class ChatHYT {
-    static Scanner scanner = new Scanner(System.in);
-    static ArrayList<Task> tasks = new ArrayList<>();
-    static ArrayList<Task> tasks_to_be_found = new ArrayList<>();
-    static Storage storage = new Storage("./data/duke.txt");
+    private static final Scanner scanner = new Scanner(System.in);
+    private static Storage storage = new Storage("./data/duke.txt");
+    private static TaskList taskList = new TaskList(storage.load());
+
 
     public static void main(String[] args) {
-        tasks = storage.load();
-        Ui.showWelcome();
+        taskList = new TaskList(storage.load());
+        System.out.println(Ui.showWelcome());
 
-        while(true) {
+        while (true) {
             String input = scanner.nextLine();
-
-            if (input.equals("bye")) {
-                Ui.showGoodbye();
-                break;
-            }
-
-            if (input.equalsIgnoreCase("list")) {
-                Ui.showList(tasks);
-                continue;
-            }
-
-            if (input.startsWith("mark ")) {
-                try {
-                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                    if (index >= 0 && index < tasks.size()) {
-                        tasks.get(index).markAsDone();
-                        storage.save(tasks);
-                        Task taskToBeMarked = tasks.get(index);
-                        Ui.showMark(taskToBeMarked);
-                    } else if (index > tasks.size()){
-                        throw new InvalidCommandException("Invalid task number.");
-                    } else {
-                        throw new EmptyDescriptionException("What?");
-                    }
-                } catch (InvalidCommandException | EmptyDescriptionException e) {
-                    System.out.println(e.getMessage());
-                }
-                continue;
-            }
-
-            if (input.startsWith("unmark ")) {
-                try {
-                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                    if (index >= 0) {
-                        tasks.get(index).unmarkAsDone();
-                        storage.save(tasks);
-                        Task taskToBeUnmarked = tasks.get(index);
-                        Ui.showUnmark(taskToBeUnmarked);
-                    } else if (index > tasks.size()){
-                        throw new InvalidCommandException("Invalid task number.");
-                    } else {
-                        throw new EmptyDescriptionException("What?");
-                    }
-                } catch (InvalidCommandException | EmptyDescriptionException e) {
-                    System.out.println("OOPS!!! " + e.getMessage());
-                }
-                continue;
-            }
-
             try {
-                if (input.startsWith("todo")) {
-                    String description = input.substring(4);
-                    if (description.isEmpty()) {
-                        throw new EmptyDescriptionException("Haha! You don't want to do anything right?");
-                    }
-                    tasks.add(new Todo(description));
-                    storage.save(tasks);
-                    Task addedTask = tasks.get(tasks.size() - 1);
-                    Ui.showAddedTask(addedTask, tasks.size());
-
-                } else if (input.startsWith("deadline")) {
-                    String input_deadline = input.substring(8);
-                    if (!input_deadline.contains("/by")) {
-                        throw new InvalidCommandException("Deadline must have an end.");
-                    }
-                    String description = input_deadline.split(" /by")[0];
-                    String by = input_deadline.split(" /by")[1];
-                    tasks.add(new Deadline(description, by));
-                    storage.save(tasks);
-                    Task addedTask = tasks.get(tasks.size() - 1);
-                    Ui.showAddedTask(addedTask, tasks.size());
-
-                } else if (input.startsWith("event")) {
-                    String input_event = input.substring(5);
-                    if (!input_event.contains("from")||!input_event.contains("to")) {
-                        throw new InvalidCommandException("Event must have a period.");
-                    }
-                    String description = input_event.split(" /from")[0];
-                    String from_1 = input_event.split("/from ")[1];
-                    String from = from_1.split(" /to")[0];
-                    String to = input_event.split("/to ")[1];
-                    tasks.add(new Event(description, from, to));
-                    storage.save(tasks);
-                    Task addedTask = tasks.get(tasks.size() - 1);
-                    Ui.showAddedTask(addedTask, tasks.size());
-
-                } else if (input.startsWith("delete")) {
-                    try {
-                        int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                        if (index >= 0) {
-                            Task taskToBeDeleted = tasks.get(index);
-                            tasks.remove(index);
-                            storage.save(tasks);
-                            Ui.showRemovedTask(taskToBeDeleted, tasks.size());
-                        } else if (index > tasks.size()){
-                            throw new InvalidCommandException("Invalid task number.");
-                        } else {
-                            throw new EmptyDescriptionException("What?");
-                        }
-                    } catch (InvalidCommandException | EmptyDescriptionException e) {
-                        System.out.println(e.getMessage());
-                    }
-                } else if (input.startsWith("find")) {
-                        String keyword = input.substring(4);
-                        for (int i = 0; i < tasks.size(); i++) {
-                            if (tasks.get(i).getDescription().contains(keyword)) {
-                                tasks_to_be_found.add(tasks.get(i));
-                            }
-                        }
-                    Ui.showListToBeFound(tasks_to_be_found);
-                } else {
-                    throw new InvalidCommandException("What?");
+                Command command = Parser.parse(input);
+                command.execute(taskList.getAllTasks(), storage, new Ui());
+                if (command.isExit()) {
+                    break;
                 }
-            } catch (EmptyDescriptionException | InvalidCommandException e) {
+            } catch (Exception e) {
                 Ui.showError(e.getMessage());
             }
         }
+
         scanner.close();
+    }
+
+    /**
+     * Generates a response for the user's chat message (for GUI mode)
+     */
+    public String getResponse(String input) {
+        try {
+            Command command = Parser.parse(input);
+            String response = command.execute(taskList.getAllTasks(), storage, new Ui());
+            return response;
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
     }
 }
