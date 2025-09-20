df9e06912e65141969aa7c037b77cdae3d363b16
yatihe01 <yatihe769@gmail.com>
2025-09-20 22:29:18 +0800

    Change DeadlineTest class.
    It did not update with my new deadline class.
    
    Let's fix it by using different testing methods.

--- src/test/java/duke/DeadlineTest.java
+++ src/test/java/duke/DeadlineTest.java
@@ -5,18 +5,25 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class DeadlineTest {
 
     @Test
-    public void deadline_parseDateCorretly() {
-        Deadline deadline1 = new Deadline("return book", "2025-08-30");
-        Assertions.assertEquals(LocalDate.of(2025, 8, 30), deadline1.getBy());
+    public void deadline_parseDateTimeCorrectly() {
+        Deadline deadline1 = new Deadline("return book", "2025-08-30 18:00");
+        Assertions.assertEquals(
+                LocalDateTime.of(2025, 8, 30, 18, 0),
+                deadline1.getBy()
+        );
     }
 
     @Test
     public void deadline_toStringFormatsCorrectly() {
-        Deadline deadline2 = new Deadline("get 2103 ip done", "2025-09-05");
-        Assertions.assertEquals("[D][ ] get 2103 ip done (by: Sep 05 2025)", deadline2.toString());
+        Deadline deadline2 = new Deadline("get 2103 ip done", "2025-09-05 23:59");
+        Assertions.assertEquals(
+                "[D][ ] get 2103 ip done (by: Sep 5 2025 23:59)",
+                deadline2.toString()
+        );
     }
 }
